b056118dfe45813e61a9c7e2af7d3e19ac831fa1
Evgenii_Gordienko <evgenii_gordienko@epam.com>
2019-02-11 16:20:15 +0300

    feat(task1): made templates and markup with styling

---
+++ src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainPageComponent } from './pages/main-page/main-page.component';
+import { EditArticlePageComponent } from './pages/edit-article-page/edit-article-page.component';
+import { ArticleComponent } from './components/article/article.component';
+import { ArticleListComponent } from './components/article-list/article-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainPageComponent,
+    EditArticlePageComponent,
+    ArticleComponent,
+    ArticleListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
